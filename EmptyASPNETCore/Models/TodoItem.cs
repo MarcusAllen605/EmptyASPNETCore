04231015a965e6c77dbfff34a561b83dc0aa0790
Marcus Allen <marcus.allen@techno-manic.com>
2023-04-25 13:39:23 -0400

    Add project files.

---
+++ EmptyASPNETCore/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace EmptyASPNETCore.Models;
+
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool isComplete { get; set; }
+        public string? Secret { get; set; }
+    }
+
